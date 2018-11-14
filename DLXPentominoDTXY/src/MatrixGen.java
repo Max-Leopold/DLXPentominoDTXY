0ed05d21b71a4bcee9600a87c394dc5a93b4bc4f
Maximilian Leopold <max.leopold2111@gmail.com>
2018-11-14 14:24:17 +0100

    Added Header Generation

--- DLXPentominoDTXY/src/MatrixGen.java
+++ DLXPentominoDTXY/src/MatrixGen.java
@@ -4,9 +4,12 @@ import java.util.LinkedList;
 
 public class MatrixGen {
 
-  int n = 20;
+  int n = 200;
   int[][] matrix;
   LinkedList<int[]> positions;
+  DLXNode h;
+  DLXNode[] columnHeader;
+  int cnt = 0;
 
   public MatrixGen() {
 
@@ -20,6 +23,15 @@ public class MatrixGen {
     genX();
     genY();
     genT();
+
+    h = new DLXNode();
+    h.posX = 0;
+    h.posY = 0;
+
+    columnHeader = new DLXNode[n + 1];
+    columnHeader[0] = h;
+
+    genHeader();
     printPositions();
     System.out.println("\nAnzahl möglicher Positionen: " + positions.size());
   }
@@ -157,6 +169,123 @@ public class MatrixGen {
       }
     }
 
+  }
+
+  public void genHeader(){
+
+    for (int i = 1; i <= n; i++) {
+
+      DLXNode nextHeader = new DLXNode();
+
+      if((h.R == h) && (h.L == h)){
+
+        h.R = nextHeader;
+        h.L = nextHeader;
+
+        nextHeader.L = h;
+        nextHeader.R = h;
+      } else {
+
+        DLXNode lastElement = h.L;
+
+        h.L = nextHeader;
+        nextHeader.R = h;
+
+        nextHeader.L = lastElement;
+        lastElement.R = nextHeader;
+      }
+
+      nextHeader.posY = 0;
+      nextHeader.posX = i;
+      columnHeader[i] = nextHeader;
+    }
+  }
+
+
+  //-- Methoden von Herr Professor Heinz --
+
+  /**
+   * Class DLXNode
+   *   represents a matrix element of the cover matrix with value 1
+   *   links go to up down left right neigbors, and column header
+   *   can also be used as colm header or root of column headers
+   *   matrix is sparsely coded
+   *   try to do all operations very efficiently
+   *   see:
+   *      http://en.wikipedia.org/wiki/Dancing_Links
+   *      http://arxiv.org/abs/cs/0011047
+   */
+  class DLXNode{       // represents 1 element or header
+    DLXNode C;           // reference to column-header
+    DLXNode L, R, U, D;  // left, right, up, down references
+    DLXNode(){ C=L=R=U=D=this; }
+    int posX;
+    int posY;
+
+    @Override
+    public String toString() {
+      return "X: " + posX + ", Y: " + posY;
+    }
+
+    // supports circular lists
+  }
+
+  /**
+   * search tries to find and count all complete coverings of the DLX matrix.
+   *        Is a recursive, depth-first, backtracking algorithm that finds
+   *        all solutions to the exact cover problem encoded in the DLX matrix.
+   *        each time all columns are covered, static long cnt is increased
+   * @param k: number of level
+   *
+   */
+  public void search (int k){ // finds & counts solutions
+    if (h.R == h) {cnt++; return;}     // if empty: count & done
+    DLXNode c = h.R;                   // choose next column c
+    cover(c);                          // remove c from columns
+    for (DLXNode r=c.D; r!=c; r=r.D){  // forall rows with 1 in c
+      for (DLXNode j=r.R; j!=r; j=j.R) // forall 1-elements in row
+        cover(j.C);                    // remove column
+      search(k+1);                    // recursion
+      for (DLXNode j=r.L; j!=r; j=j.L) // forall 1-elements in row
+        uncover(j.C);                  // backtrack: un-remove
+    }
+    uncover(c);                        // un-remove c to columns
+  }
+
+  /**
+   * cover "covers" a column c of the DLX matrix
+   *       column c will no longer be found in the column list
+   *       rows i with 1 element in column c will no longer be found
+   *       in other column lists than c
+   *       so column c and rows i are invisible after execution of cover
+   * @param c: header element of column that has to be covered
+   *
+   */
+  public void cover (DLXNode c){ // remove column c
+    c.R.L = c.L ;                         // remove header
+    c.L.R = c.R ;                         // .. from row list
+    for (DLXNode i=c.D; i!=c; i=i.D)      // forall rows with 1
+      for (DLXNode j=i.R; i!=j; j=j.R){   // forall elem in row
+        j.D.U = j.U ;                     // remove row element
+        j.U.D = j.D ;                     // .. from column list
+      }
+  }
+
+  /**
+   * uncover "uncovers" a column c of the DLX matrix
+   *       all operations of cover are undone
+   *       so column c and rows i are visible again after execution of uncover
+   * @param c: header element of column that has to be uncovered
+   *
+   */
+  public void uncover (DLXNode c){//undo remove col c
+    for (DLXNode i=c.U; i!=c; i=i.U)      // forall rows with 1
+      for (DLXNode j=i.L; i!=j; j=j.L){   // forall elem in row
+        j.D.U = j ;                       // un-remove row elem
+        j.U.D = j ;                       // .. to column list
+      }
+    c.R.L = c ;                           // un-remove header
+    c.L.R = c ;                           // .. to row list
   }
 }
 
